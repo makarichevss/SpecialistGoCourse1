91fa19c41ea0589c39d2d9d434d599d4088dd6f1
Sergey Makarichev <harleymack@mail.ru>
2021-09-14 16:02:57 +0300

    first commit

--- src/main.go
+++ src/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	myName := "Vasyl"
-	fmt.Println(myName)
-	fmt.Printf("Hello, my name is %s\n", myName)
+	var name string
+	var age int
+
+	fmt.Print("Enter your name: ") // Writing a request message to the stdout 	// Reading from the stdin into the name variable
+	fmt.Print("")                  // Going to the next line by writing /n to the stdout
+
+	fmt.Print("Enter your age: ") // Writing a request message to the stdout 	// Reading from the stdin into the age variable
+	fmt.Print("")                 // Going to the next line by writing /n to the stdout
+
+	fmt.Print(name, age) // Writing to the stdout the values of name and
+	// age variables that you have entered
 }
